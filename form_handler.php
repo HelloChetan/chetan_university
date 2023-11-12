1e04c66e6223b6fa754f41f67edc34d1af79c7a0
Chetansonar11 <121669242+Chetansonar11@users.noreply.github.com>
2023-11-12 15:54:28 +0530

    Add files via upload

---
+++ form_handler.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$email_from ='chetansonar2003@gmail.com';
+$email_subject ='New Form Submission';
+$email_body ="User Name: $name.\n."
+             "User Email: $visitor_email.\n."
+             "Subject: $subject.\n."
+             "Message: $message.\n";
+
+$to = 'sonarc69@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .="Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
